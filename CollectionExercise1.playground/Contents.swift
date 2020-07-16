24a3ff25bfc7938e0fa1e3a206fdf02bd61f10c3
Jose Sanchez <josersanchez0117@gmail.com>
2020-07-16 13:15:18 -0500

    Finished Exercise 1

---
+++ CollectionExercise1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+let moneyCollected = [
+    "One Dollar Bill", "Five Dollar Bill", "Ten Dollar Bill", "Hundred Dollar Bill", "Fifty Cent Coin", "Penny Coin", "One Mexican Peso", "Dollar Coin", "Two Dollar Bill", "Ten Cent Coin", "Twenty-five Cent Coin"
+]
+
+var str = "Hello, playground"
